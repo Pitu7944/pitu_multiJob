c782dfc4713753c78e43d59e46f3ac8945ebca66
Pitu7944 <Pitu7944@gmail.com>
2020-10-14 16:53:16 +0200

    initial rel

---
+++ Config.lua
@@ -0,0 +1,28 @@
+--[[ edited ("insert date here") by :
+ ___  _    _        ___  ___   __    __          ___  ___  _  _ 
+| . \<_> _| |_ _ _ |_  || . | /. |  /. |  _|_|_ <_  >|_  |/ |/ |
+|  _/| |  | | | | | / / `_  //_  .|/_  .| _|_|_  / /  / / | || |
+|_|  |_|  |_| `___|/_/   /_/   |_|   |_|   | |  <___>/_/  |_||_|
+any issues? Dm me on discord (Pitu7944#2711)
+]]
+
+Config = {}
+Config.Jobs = {
+    {
+        name = 'testjob'
+        Blips = {
+            {
+                color = 1
+                id = 1
+                pos = {x = 123, y = 456, z = 789}
+                label = 'testBlip'
+            }
+        }
+        Zones = {
+            changerooms = {
+                enabled = true
+                pos = {x = 123, y = 456, z = 789}
+            }
+        }
+    }
+}
---
+++ server/server.lua
@@ -0,0 +1,29 @@
+--[[ edited ("insert date here") by :
+ ___  _    _        ___  ___   __    __          ___  ___  _  _ 
+| . \<_> _| |_ _ _ |_  || . | /. |  /. |  _|_|_ <_  >|_  |/ |/ |
+|  _/| |  | | | | | / / `_  //_  .|/_  .| _|_|_  / /  / / | || |
+|_|  |_|  |_| `___|/_/   /_/   |_|   |_|   | |  <___>/_/  |_||_|
+any issues? Dm me on discord (Pitu7944#2711)
+]]
+
+-- settings -- 
+local debug_enable = true
+-- code here :
+
+print("loaded")
+
+-- debug functions:
+
+function dprint(txt)
+    if debug_enable == true then
+        print("^4[^2"..GetCurrentResourceName().."^4] ^3"..tostring(txt)..'^0')
+    end
+end
+
+--[[ edited ("insert date here") by :
+ ___  _    _        ___  ___   __    __          ___  ___  _  _ 
+| . \<_> _| |_ _ _ |_  || . | /. |  /. |  _|_|_ <_  >|_  |/ |/ |
+|  _/| |  | | | | | / / `_  //_  .|/_  .| _|_|_  / /  / / | || |
+|_|  |_|  |_| `___|/_/   /_/   |_|   |_|   | |  <___>/_/  |_||_|
+any issues? Dm me on discord (Pitu7944#2711)
+]]
